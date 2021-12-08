9870d1b7911da85daa1fc8cd99aebf4f4ee54fbc
Blake <reyno254@unlv.nevada.edu>
2021-12-07 20:44:18 -0800

    add cli arguments

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"strings"
+	"unicode"
 
 	"github.com/br7552/lsystem/lsys"
 )
 
 func main() {
-	l := lsys.NewLsystem("F", "F", "F-F++F-F")
+	var (
+		depth int
+		axiom string
+		rules []string
+	)
 
-	for i := 0; i < 3; i++ {
+	flag.IntVar(&depth, "depth", 3, "Number of iterations")
+	flag.StringVar(&axiom, "axiom", "", "Lsystem axiom")
+	flag.Func("rules", "Lsystem production rules", func(s string) error {
+		rules = strings.FieldsFunc(s, func(c rune) bool {
+			return unicode.IsSpace(c) || c == '='
+		})
+		return nil
+	})
+
+	flag.Parse()
+
+	l := lsys.NewLsystem(axiom, rules...)
+
+	for i := 0; i < depth; i++ {
 		l.Grow()
 		fmt.Println(l)
 	}
