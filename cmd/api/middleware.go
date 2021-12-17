da4117b31500992cee72a4e1f51e666666032413
Blake <reyno254@unlv.nevada.edu>
2021-12-17 12:36:05 -0800

    allow all cross-origin requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -83,3 +83,11 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		next.ServeHTTP(w, r)
+	})
+}
